c896827e53538ae57aa4948823f94d048a965748
Luan Cardoso <72113891+luancardosoti@users.noreply.github.com>
2022-01-22 03:42:45 -0300

    Adicionando bloco na Blockchain

--- blockchain.js
+++ blockchain.js
@@ -5,4 +5,15 @@ class BlockChain {
   constructor() {
     this.chain = [Block.genesis()];
   }
+
+  addBlock(data) {
+    const lastBlock = this.chain[this.chain.length - 1];
+    const block = mineBlock(lastBlock, data);
+    this.chain.push(block);
+
+    return block;
+  }
 }
+
+
+module.exports = BlockChain;
